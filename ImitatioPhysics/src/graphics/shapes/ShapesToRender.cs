3aebde7003a1f43afb483fcf20e780f39dcc8cc0
WumbaLumba <maria.ghent03@gmail.com>
2022-03-08 13:39:35 +0000

    added a class for storing all shapes using a dictionary + a way of selecting shapes using their label (string)

---
+++ ImitatioPhysics/src/graphics/shapes/ShapesToRender.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ImitatioPhysics
+{
+    class ShapesToRender
+    {
+        //private List<Shape> _shapes = new List<Shape>();
+        private Dictionary<string, Shape> _shapes = new Dictionary<string, Shape>();
+
+        public ShapesToRender()
+        {
+            
+        }
+
+        public void AddShape(string label, Shape shape)
+        {
+            _shapes.Add(label, shape);
+        }
+
+        public Shape SelectShape(string label)
+        {
+            // Extract the key where the element value is equal to the label
+            return _shapes.FirstOrDefault(x => x.Key == label).Value;
+        }
+    }
+}
